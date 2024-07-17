9f31139c91dec84e1dd95f4aed54bb2784ab2ae1
snakeJ11 <jeferson.md0212@gmail.com>
2024-07-16 22:15:56 -0400

    all roman(fix img bug)

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -93,7 +93,7 @@ const updateUser = async (req: Request, res: Response) => {
     if(user.banner==null){ banner=bannerUrl;}//si no hay banner en firebase
     else if(req.files['banner'] != user.banner ){deleteImage(user.banner); banner=bannerUrl;}//si hay banner en firebase
     else if(!data.banner && req.files['banner'] == null && user.banner!=null){deleteImage(user.banner); banner=null;}//si se queda sin banner
-    else if(data.banner){banner=data.banner;}//dejar img
+    else if(data.banner){banner=data.banner;}//dejar banner
 
     if(user.img==null){profilePictur=imgUrl;} //si no hay img en firebase
     else if(data.img != user.img ){deleteImage(user.img);profilePictur=imgUrl;}//si hay img en firebase
