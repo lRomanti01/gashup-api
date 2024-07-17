5d6c1b1252c49cf8cb6b872732330214acf9cb59
snakeJ11 <jeferson.md0212@gmail.com>
2024-07-16 20:00:24 -0400

    jef

--- src/routes/community.routes.ts
+++ src/routes/community.routes.ts
@@ -5,11 +5,7 @@ import { upload } from "../multer";
 const communityRouter: express.Router = express.Router();
 
 //post
-communityRouter.post(
-  "/createChatCommunity",
-  upload,
-  community.createChatCommunity
-);
+communityRouter.post("/createChatCommunity",upload,community.createChatCommunity);
 communityRouter.post("/createCommunity", upload, community.createCommunity);
 communityRouter.post("/joinChatCommunity/:_id", community.joinChatCommunity);
 communityRouter.post("/joinCommunity/:_id", community.joinCommunity);
@@ -17,11 +13,7 @@ communityRouter.post("/joinCommunity/:_id", community.joinCommunity);
 //put
 communityRouter.put("/updateCommunity/:_id", upload, community.updateCommunity);
 communityRouter.put("/assignAdmins/:_id", community.assignAdmins);
-communityRouter.put(
-  "/updateCommunityChat/:_id",
-  upload,
-  community.updateCommunityChat
-);
+communityRouter.put("/updateCommunityChat/:_id",upload,community.updateCommunityChat);
 
 ///delete
 communityRouter.delete("/deleteCommunity/:_id", community.deleteCommunity);
@@ -35,7 +27,7 @@ communityRouter.get("/getCommunities", community.getCommunities);
 communityRouter.get("/getCommunity/_id", community.getCommunity);
 communityRouter.get("/getCommunitiesForCategories",community.getCommunitiesForCategories);
 communityRouter.get("/getCommunityChats/:_id", community.getCommunityChats);
-communityRouter.get("/hotCommunity", community.hotCommunity);
+communityRouter.get("/hotCommunity", community.hotCommunity);// documentar
 
 
 export default communityRouter;
