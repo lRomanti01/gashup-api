871a13f1f70da07687ca312a4111029a17cf3990
snakeJ11 <jeferson.md0212@gmail.com>
2025-02-04 19:07:58 -0400

    vercel up

--- src/app.ts
+++ src/app.ts
@@ -25,3 +25,5 @@ app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port  ${process.env.PORT}`);
 });
+
+export default app;
